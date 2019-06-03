165ae19a1af44a71ca615bd007da389650669d95
gopiteekenam <>
2019-06-03 19:17:55 +0530

    Committed the files for Scala

---
+++ PlayScalaForms/PlayScalaForms/app/controllers/Traversing.scala
@@ -0,0 +1,20 @@
+package controllers
+
+case class User(
+                 id: Int,
+                 firstName: String,
+                 lastName: String,
+                 age: Int,
+                 gender: Option[String] )
+
+object Traversing{
+  def main(args : Array[String]){
+    val users  = List(User(1, "Scala","World",40, Some("male")),
+      User(2, "Java","World",42, Some("female")))
+    println(users.filter((user: User) => user.firstName.contains("S")))
+    println(users.filter((user: User) => user.lastName.contains("S")))
+    println(users.filter(_.firstName.contains("S")))
+
+  }
+
+}
