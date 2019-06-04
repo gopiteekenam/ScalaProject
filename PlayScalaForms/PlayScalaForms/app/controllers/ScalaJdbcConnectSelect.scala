524dbb20574ea934bc650827bbbcff257c91ca16
gopiteekenam <>
2019-06-04 11:42:50 +0530

    Committed the files for Scala

--- PlayScalaForms/PlayScalaForms/app/controllers/ScalaJdbcConnectSelect.scala
+++ PlayScalaForms/PlayScalaForms/app/controllers/ScalaJdbcConnectSelect.scala
@@ -25,13 +25,20 @@ object ScalaJdbcConnectSelect {
 
       // create the statement, and run the select query
       val statement = connection.createStatement()
-      statement.execute("insert into candidate (name, city,programing_language) values('sateesh v','renugunta','multi language')")
+//      statement.execute("insert into candidate (name, city,programing_language) values('sateesh v','renugunta','multi language')")
       val resultSet = statement.executeQuery("SELECT * from CANDIDATE")
-      val names = Iterator.from(0).takeWhile(_ => resultSet.next()).map(_ => {
+      val employees = Iterator.from(0).takeWhile(_ => resultSet.next()).map(_ => {
         Employee(resultSet.getString("CITY"), resultSet.getString("PROGRAMING_LANGUAGE"))
       }).toList
-      println(names)
+      println("iterating the list "+employees.foreach(print))
+      println()
+      println("list of all employees containing scala "+employees.filter((employee: Employee) => employee.language.contains("scala")))
 
+      for(employee <- employees) println(employee.language)
+
+      println("map"+employees.map(employee => employee.name))
+
+      println("Sorting list"+employees.sortBy(employee => employee.language))
 
 
     } catch {
